9c253ef888f825a2ab2403de93916e7eb6e590cf
EugeniaGramajo <gramajoeugenia@gmail.com>
2023-01-02 14:50:23 -0300

    index.js

--- client/src/store/store.js
+++ client/src/store/store.js
@@ -2,13 +2,7 @@ import { createStore, applyMiddleware, compose } from 'redux';
 import reducer from '../reducers/reducers';
 import thunk from 'redux-thunk';
 
-const composeEnhancers =
-   (window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__) ||
-   compose;
 
-const store = createStore(
-  reducer,
-   composeEnhancers(applyMiddleware(thunk)),
-);
+const store = createStore(reducer,applyMiddleware(thunk));
 
 export default store;
