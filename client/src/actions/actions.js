14458c2378f6a9f74c0c3fb7f1ecf23981e5e04e
EugeniaGramajo <gramajoeugenia@gmail.com>
2023-01-02 13:25:27 -0300

    trying to fix

--- client/src/actions/actions.js
+++ client/src/actions/actions.js
@@ -17,9 +17,7 @@ export const getAllRecipes = (param) => {
   console.log("entre al llamado")
   return function (dispatch) {
     return axios
-      .get("deploy-food-production.up.railway.app/recipes",{ headers:{
-        "accept-encoding": null
-      }})
+      .get("deploy-food-production.up.railway.app/recipes")
       .then((response) =>
       
         dispatch(console.log(response.data),{ type: GET_ALL_RECIPES, payload: response.data })
