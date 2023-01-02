1032f974f2cbeafdf7abd821bb591642413ce013
EugeniaGramajo <gramajoeugenia@gmail.com>
2023-01-02 19:53:38 -0300

    dfgdgffgd

--- api/src/helpers/RecipeCont/GetRecipeById/getRecipeByApiId.js
+++ api/src/helpers/RecipeCont/GetRecipeById/getRecipeByApiId.js
@@ -2,25 +2,24 @@ const axios = require("axios");
 const getAllRecipes = require("../GetRecipes/GetAllRecipes");
 
 const getRecipeByApiId = async (id) => {
-    try {
-      const recipe = await getAllRecipes()
-
-      let foundId = recipe.data.results.filter(e =>{ 
-        console.log(e)
-        e.id=id})
-      console.log(foundId)
-      return {
-        id: foundId.id,
-        title: foundId.title,
-        summary: foundId.summary.replace(/<[^>]*>?/g, ''),
-        healthScore: foundId.healthScore,
-        steps: foundId.analyzedInstructions[0]?.steps.map((step) => {
-          return step.step;
-        }),
-        dishTypes: foundId.dishTypes,
-        diet: foundId.diets.map(diet=>({name:diet})),
-        readyInMinutes: foundId.readyInMinutes,
-        ingredients: foundId.analyzedInstructions[0]?.steps
+  try {
+    const recipes = await axios.get(
+      `https://run.mocky.io/v3/84b3f19c-7642-4552-b69c-c53742badee5`
+    );
+    const foundId = recipes.data.results.find((recipe) => recipe.id === id);
+    console.log(foundId);
+    return {
+      id: foundId.id,
+      title: foundId.title,
+      summary: foundId.summary.replace(/<[^>]*>?/g, ""),
+      healthScore: foundId.healthScore,
+      steps: foundId.analyzedInstructions[0]?.steps.map((step) => {
+        return step.step;
+      }),
+      dishTypes: foundId.dishTypes,
+      diet: foundId.diets.map((diet) => ({ name: diet })),
+      readyInMinutes: foundId.readyInMinutes,
+      ingredients: foundId.analyzedInstructions[0]?.steps
         .map((each) => {
           return each.ingredients;
         })
@@ -31,11 +30,11 @@ const getRecipeByApiId = async (id) => {
         )
         .join(",")
         .split(","),
-        servings: foundId.servings,
-        image: foundId.image,
-      };
-    } catch (e) {
-      return e;
-    }
-  };
-  module.exports=getRecipeByApiId;
+      servings: foundId.servings,
+      image: foundId.image,
+    };
+  } catch (e) {
+    return e;
+  }
+};
+module.exports = getRecipeByApiId;
