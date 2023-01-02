0439464d5377eae1eea2a8999359f980ddd486b9
EugeniaGramajo <gramajoeugenia@gmail.com>
2023-01-02 16:34:03 -0300

    asdasd

--- client/src/actions/actions.js
+++ client/src/actions/actions.js
@@ -9,41 +9,44 @@ export const GET_DIETS = "GET_DIETS";
 export const GET_RECIPES_SORT = "GET_RECIPES_SORT";
 export const GET_RECIPES_BY_DIET = "GET";
 export const CLEAN_STATE = "CLEAN_STATE";
-export const GET_TITLE_RECIPES = "GET_TITLE_RECIPES";
-let URL = "deploy-food-production.up.railway.app/";
+export const GET_TITLE_RECIPES="GET_TITLE_RECIPES"
 
-export const getAllRecipes = () => (dispatch) => {
-  return fetch("https://deploy-food-production.up.railway.app/recipes")
-    .then((res) => res.json())
-    .then((data) => {
-      return dispatch({ type: GET_ALL_RECIPES, payload: data });
-    })
-    .catch((error) => {
-      console.log(error);
-    });
+
+
+export const getAllRecipes = (param) => {
+  console.log("entre al llamado")
+  return function (dispatch) {
+    return axios
+      .get("https://deploy-food-production.up.railway.app/recipes",{ headers:{
+        "accept-encoding": null
+      }})
+      .then((response) =>
+      
+        dispatch(console.log(response.data),{ type: GET_ALL_RECIPES, payload: response.data })
+      );
+  };
 };
 
 export const getAllRecipesById = (id) => {
-  return async function (dispatch) {
-    const res = await axios.get(
-      `deploy-food-production.up.railway.app/recipes/${id}`
-    );
-    return dispatch({ type: GET_ALL_RECIPES_BY_ID, payload: res.data });
+  return function (dispatch) {
+    return axios
+      .get(`https://deploy-food-production.up.railway.app/recipes/${id}`)
+      .then((res) =>
+        dispatch({ type: GET_ALL_RECIPES_BY_ID, payload: res.data })
+      );
   };
 };
 
 export const getDietTypes = () => {
-  return async function (dispatch) {
-    try {
-      const res = await axios.get(
-        "deploy-food-production.up.railway.app/diets"
-      );
-      console.log(res);
-      console.log(res.data);
-      dispatch({ type: GET_DIETS, payload: res.data });
-    } catch (err) {
-      return console.log(err);
-    }
+  return function (dispatch) {
+        return axios.get("https://deploy-food-production.up.railway.app/diets")
+    .then(res=> {
+      console.log(res)
+      console.log(res.data)
+      dispatch({ type: GET_DIETS, payload: res.data })
+      
+    })
+    .catch(err=>console.log(err))
     /* return fetch("http://localhost:3001/diets")
       .then((response) => response.json())
       .then((data) => {
@@ -67,24 +70,27 @@ export const cleanState = () => {
 };
 
 export const getRecipeByName = (payload) => {
+ 
   return async function (dispatch) {
     try {
-      var json = await axios.get("recipes?title=" + payload);
+      
+      var json = await axios.get(
+        "recipes?title=" + payload
+      );
       return dispatch({
         type: GET_TITLE_RECIPES,
         payload: json.data,
       });
     } catch (error) {
-      return <NotFound></NotFound>;
+     return <NotFound></NotFound>
     }
   };
 };
 
 export const getRecipesSort = (sort) => {
-  return {
-    type: GET_RECIPES_SORT,
-    payload: sort,
-  };
+  return{
+    type: GET_RECIPES_SORT, payload: sort
+  }
 };
 
 export const getRecipesByScore = () => {};
@@ -95,10 +101,7 @@ export const updateRecipe = () => {};
 
 export const createRecipe = (payload) => {
   return async function (dispatch) {
-    var json = await axios.post(
-      "deploy-food-production.up.railway.app/recipes",
-      payload
-    );
+    var json = await axios.post("https://deploy-food-production.up.railway.app/recipes", payload);
     return json;
   };
 };
