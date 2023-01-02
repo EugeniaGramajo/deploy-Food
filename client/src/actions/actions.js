72969c060ab26566fbd3d24d2e9ea7ddb9233b70
EugeniaGramajo <gramajoeugenia@gmail.com>
2023-01-02 16:39:57 -0300

    nuevo error

--- client/src/actions/actions.js
+++ client/src/actions/actions.js
@@ -17,9 +17,7 @@ export const getAllRecipes = (param) => {
   console.log("entre al llamado")
   return function (dispatch) {
     return axios
-      .get("https://deploy-food-production.up.railway.app/recipes",{ headers:{
-        "accept-encoding": null
-      }})
+      .get("https://deploy-food-production.up.railway.app/recipes")
       .then((response) =>
       
         dispatch(console.log(response.data),{ type: GET_ALL_RECIPES, payload: response.data })
