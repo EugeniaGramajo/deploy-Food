f732c4dcb6875670942eac2a81f1c888b1287fca
EugeniaGramajo <gramajoeugenia@gmail.com>
2023-01-02 18:12:35 -0300

    changes api

--- api/src/helpers/RecipeCont/GetRecipeById/getRecipeByApiId.js
+++ api/src/helpers/RecipeCont/GetRecipeById/getRecipeByApiId.js
@@ -6,7 +6,7 @@ const { API_KEY3 } = process.env;
 const getRecipeByApiId = async (id) => {
     try {
       const recipe = await axios.get(
-        `https://api.spoonacular.com/recipes/${id}/information?apiKey=${API_KEY3}`
+        `https://run.mocky.io/v3/84b3f19c-7642-4552-b69c-c53742badee5/${id}`
       );
       const foundId = recipe.data;
       return {
