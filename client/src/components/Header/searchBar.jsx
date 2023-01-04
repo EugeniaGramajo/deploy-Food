b88fb98859735604d91f9bf1ed27150fcfbe2148
EugeniaGramajo <gramajoeugenia@gmail.com>
2023-01-04 16:06:29 -0300

    history

--- client/src/components/Header/searchBar.jsx
+++ client/src/components/Header/searchBar.jsx
@@ -1,24 +1,26 @@
 import React from "react";
 import { useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
-import { Link } from "react-router-dom";
+import { Link, useHistory } from "react-router-dom";
 import { getAllRecipes, getRecipeByName } from "../../actions/actions";
 import styles from "./stylesComponents/searchBar.module.css"
 
 export default function SearchBar (){
 
+  const history=useHistory()
   const [search, setSearch]=useState("")
   const dispatch = useDispatch()
   const state = useSelector(state=>state.recipes)
 
   const changeHandler = (e)=>{
     setSearch(e.target.value)
-    console.log(search)
+    
   }
   const clickHandler = (e)=>{
     e.preventDefault()
+    history("/home")
     dispatch(getRecipeByName(search))
-    console.log(state)
+    
   }
   const clickHandler2 = (e)=>{
     e.preventDefault()
@@ -31,7 +33,7 @@ export default function SearchBar (){
     <>
     <div>
       <input placeholder="  Search a recipe" className={styles.input} value={search} onChange={e=>{changeHandler(e)}}></input>
-      <button className={styles.button} onClick={(e)=>clickHandler(e)}> <Link to={"/home"}>Search</Link> </button>
+      <button className={styles.button} onClick={(e)=>clickHandler(e)}> Search </button>
       <button className={styles.button} onClick={(e)=>{clickHandler2(e)}}>Reset</button>
     </div>
     </>
