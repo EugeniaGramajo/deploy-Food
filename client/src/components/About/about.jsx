bdd829750fc2db227ba3e366fc886b037347dcf6
EugeniaGramajo <gramajoeugenia@gmail.com>
2023-01-04 10:31:26 -0300

    fix link

--- client/src/components/About/about.jsx
+++ client/src/components/About/about.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import styles from "./about.module.css"
-import Link from "react-router-dom"
+import {Link} from "react-router-dom"
 
 export default function About(){
     
